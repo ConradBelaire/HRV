8c5f70fa0dbed1dfb282a169a3593395b0806116
ThomasKWood <thomas.ke.wood@outlook.com>
2023-04-14 17:05:35 -0400

    add menu add profil

--- HRV/src/menu.cpp
+++ HRV/src/menu.cpp
@@ -1,23 +1,58 @@
 #include "menu.h"
 
+/**
+ * Menu constructor
+ *
+ * @param n The name of the menu
+ * @param list The list of menu items
+ * @param p The parent menu
+ */
 Menu::Menu(QString n, QStringList list, Menu* p):
     name(n), menuItems(list), parent(p) {}
 
+/**
+ * Menu destructor
+ */
 Menu::~Menu() {
     for(int x = 0; x < childMenus.length(); x++) {
         delete childMenus[x];
     }
 }
 
+// getters
+/**
+Get the parent menu
+@return parent menu as a Menu pointer
+*/
+Menu Menu::getParent() { return parent; }
+/**
+Get a specific child menu
+@param i Index of the child menu
+@return child menu at the specified index as a Menu pointer
+*/
+Menu Menu::getChildMenu(int i) { return childMenus[i]; }
+/**
+Get the name of the menu
+@return menu name as a QString
+*/
+QString Menu::getName() { return name; }
+/**
+Get a specific menu item
+@param i Index of the menu item
+@return menu item at the specified index as a QString
+*/
+QString Menu::getMenuItem(int i) { return menuItems[i]; }
+/**
+Get all menu items-
+@return menu items as a QStringList
+*/
+QStringList Menu::getMenuItems() { return menuItems; }
 
-void Menu::addChildMenu(Menu* m) {
+// methods 
+/**
+Add a child menu
+@param m Pointer to the Menu object to be added as a child
+*/
+void Menu::addChildMenu(Menu m) {
     childMenus.push_back(m);
 }
-
-
-// getters
-QString Menu::getName() { return name; }
-QStringList Menu::getMenuItems() { return menuItems; }
-QString Menu::getMenuItem(int i) { return menuItems[i]; }
-Menu* Menu::getParent() { return parent; }
-Menu* Menu::getChildMenu(int i) { return childMenus[i]; }
--- HRV/src/profile.cpp
+++ HRV/src/profile.cpp
@@ -1,11 +1,44 @@
 #include "profile.h"
-
+/**
+ * Profile constructor
+ *
+ * @param id The profile ID
+ * @param batteryLvl The battery level
+ * @param sessionAmt The amount of sessions
+ */
 Profile::Profile(int id, double batteryLvl, int sessionAmt):
     id(id), batteryLvl(batteryLvl), sessionAmt(sessionAmt) {}
 
-// getters
-int Profile::getId() { return id; }
-double Profile::getBLvl() { return batteryLvl; }
+// setters
+/**
+ * Set the battery level
+ *
+ * @param newLvl The new battery level
+ */
 void Profile::setBLvl(double newLvl) { batteryLvl = newLvl; }
-int Profile::getSessAmt() { return sessionAmt; }
+/**
+ * Increase the session amount by one
+ *
+ * @return The updated session amount
+ */
 int Profile::increaseSessAmt() { return sessionAmt++; }
+
+// getters
+/**
+ * Get the profile ID
+ *
+ * @return The profile ID
+ */
+int Profile::getId() { return id; }
+/**
+ * Get the session amount
+ *
+ * @return The amount of sessions
+ */
+int Profile::getSessAmt() { return sessionAmt; }
+/**
+ * Get the battery level
+ *
+ * @return The battery level
+ */
+double Profile::getBLvl() { return batteryLvl; }
